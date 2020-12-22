02a5285e1c1b3aa33a8e58abc9f7948c2b0f40dc
C. L <amautacelt@gmail.com>
2020-12-22 04:03:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["blue"]
   add_array = "arrays!"
-  array.push(add_color)
+  array.push(add_array)
 end
 
 def add_element_to_start_of_array(array, element)
