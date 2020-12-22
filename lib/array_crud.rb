90715ffa58f77910b7c72a7631670bfc0827437e
C. L <amautacelt@gmail.com>
2020-12-22 04:33:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,6 +44,6 @@ end
 
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[3] = "totally"
+  array[4] = "totally"
   p array
 end
