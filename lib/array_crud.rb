7b77d656631bb630498fe9d75c8656478707b90d
C. L <amautacelt@gmail.com>
2020-12-22 04:02:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,18 +13,18 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  colors = ["blue"]
-  colors.unshift("wow")
+  array = ["blue"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  colors = ["blue", "arrays!"]
-  colors.pop
+  array = ["blue", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  colors = ["blue", "arrays!"]
-  colors.shift
+  array = ["blue", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
