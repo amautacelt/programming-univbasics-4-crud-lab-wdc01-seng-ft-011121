17992a190f61c942a6a82d9ddb2c101b0cf23578
C. L <amautacelt@gmail.com>
2020-12-22 03:48:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  colors = ["blue", "red", "green", "purple"]
+  colors = ["blue"]
   add_color = "yellow"
   colors.push(add_color)
 end
