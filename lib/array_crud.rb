bad686fa9b8117a062d98cffa0327e38acc80ff4
C. L <amautacelt@gmail.com>
2020-12-22 04:48:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,6 @@ end
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
-  p array
 end
 
 def retrieve_last_element_from_array(array)
