37ab2df0f7d08b1bc073517773a3ab54596865b4
C. L <amautacelt@gmail.com>
2020-12-22 04:48:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,24 +36,24 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["I", "am", "really", "learning", "arrays!"]
-  array[1]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
   p array
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[-1]
   p array
 end
 
 def update_element_from_index(array, index_number, element)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[4] = "totally"
   p array
 end
