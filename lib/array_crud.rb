6ec162d55e5b2329c04e15083b64a076be021a67
C. L <amautacelt@gmail.com>
2020-12-22 04:35:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,39 +12,47 @@ def add_element_to_end_of_array(array, element)
   array = ["I", "am", "really", "learning"]
   add_array = "arrays!"
   array.push(add_array)
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.unshift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.pop
+  p array
 end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.shift
+  p array
 end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[2]
+  p array
 end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[0]
+  p array
 end
 
 def retrieve_last_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[-1]
+  p array
 end
 
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[4] = "totally"
+  p array
 end
