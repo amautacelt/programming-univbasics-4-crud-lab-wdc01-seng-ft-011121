12b992c8bcb65fd2f600a6e8f83a73364f171d52
C. L <amautacelt@gmail.com>
2020-12-22 04:24:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["wow", "I", "am", "really"]
+  array = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "I", "am", "really", "learning"]
+  array = ["I", "am", "really", "learning"]
   add_array = "arrays!"
   array.push(add_array)
 end
